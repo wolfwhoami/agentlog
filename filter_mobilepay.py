58019fd09aac2bbf731803a0b3e647c9b7af4be9
yixuanzi <yeying0311@126.com>
2016-06-21 09:38:25 +0800

    Signed-off-by: yixuanzi <yeying0311@126.com>

--- filter_mobilepay.py
+++ filter_mobilepay.py
@@ -1,6 +1,17 @@
+import os
+import sys
+import re
+
+ree=re.compile(r"^\[INFO \] \S+ \S+ \S+?>{10}:(?P<dst>\S+?)(?P<url>\/\S+).*$")
+
+
+def dict2str(dt):    
+    return "%s %s" %(dt['url'],dt['dst'])
+    
 def myfilter(lines):
     for line in lines:
         line=line.decode('utf8')
-        if line[32:41]==u'\u5ba2\u6237\u7aef\u8bf7\u6c42\u53c2\u6570\u5982\u4e0b':
-            yield line
-    
+        if line[:5]=='[INFO':
+            match=ree.match(line)
+            if match:
+                yield dict2str(match.groupdict())
--- filter_netpay.py
+++ filter_netpay.py
@@ -1,10 +1,24 @@
+import os
+import sys
 import re
-ree=re.compile(r"^\w{3} \d{2} \d{2}:\d{2}:\d{2} (?P<ip>[\w\.]+) netpay\[\d+\]: (userId=(?P<uid>\w+),userType=(?P<utp>\d))?\[[\u4e00-\u9fa5]+uri=(?P<url>[\/-\w\.]+),.*$")
 
+
+ree=re.compile(r"^\[INFO \] \S+ \S+ \S(userId\=(?P<uid>\w+),userType\=(?P<utp>\d))?\S+?uri\=(?P<url>\S+?),.*$")
+
+
+def uid2ip(uid):
+    return "%s.%s.%s.%s" %(uid[0],uid[1:4],uid[4:7],uid[7:10])
+
+def dict2str(dt):
+    if dt['uid']:
+        return "%s %s %s %s" %(dt['url'],dt['uid'],dt['utp'],uid2ip(dt['uid']))
+    else:
+        return dt['url']
+    
 def myfilter(lines):
     for line in lines:
         line=line.decode('utf8')
-        #
         if len(line)>32 and line[32]==u'\u3010':
             match=ree.match(line)
-            yield line[33:-2]
+            if match:
+                yield dict2str(match.groupdict())
